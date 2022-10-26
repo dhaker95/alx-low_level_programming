44051729a7b35a2541853aa29810303bd713e438
Dhaker Achour <achour.dhaker@gmail.com>
2022-10-26 01:58:02 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,21 +1,19 @@
 #include "main.h"
 
 /**
- * _strncpy - a function that copies a string
- * @dest: copy to
- * @src: copy from
- * @n: input number of char
+ * _strcmp - a function that compares two strings
+ * @s1: input one
+ * @s2: input two
  * Return: Always 0 (Success)
  */
-char *_strncpy(char *dest, char *src, int n)
+int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; src[i] != '\0'; i++)
-		if (i < n)
-			dest[i] = src[i];
-	while (i < n)
-		dest[i++] = '\0';
-
-	return (dest);
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
 }
