7e3d1288447db493fcb45360541037492aa1b492
Dhaker Achour <achour.dhaker@gmail.com>
2022-11-10 01:37:27 -0800

    Task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,24 +1,18 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
 /**
- * create_array - creates an array of chars.
- * @size: size of the array.
- * @c: char for initialized the array.
- *
- * Return: the string initialized.
+ * malloc_checked - Entry Point
+ * @b: input amount
+ * Return: pointer to new mem
  */
-char *create_array(unsigned int size, char c)
+void *malloc_checked(unsigned int b)
 {
-	char *arrayMem;
-	unsigned int i;
+	void *n;
 
-	if (size == 0)
-		return (NULL);
-	arrayMem = malloc(size * sizeof(char));
-	if (arrayMem == NULL)
-		return (NULL);
-	for (i = 0; i < size; i++)
-		*(arrayMem + i) = c;
-	return (arrayMem);
-}
+	n = malloc(b);
+
+	if (n == NULL)
+		exit(98);
+	return (n);
+}}
