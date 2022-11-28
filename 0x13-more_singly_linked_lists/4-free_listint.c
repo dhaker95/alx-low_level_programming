547cd33257e15f0e51d57963efa2ce7677c74a40
Dhaker Achour <achour.dhaker@gmail.com>
2022-11-28 07:22:57 -0800

    Task 4 Fix

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,34 +1,20 @@
 #include "lists.h"
-/**
- * add_nodeint_end - adds a node at the end of a list.
- * @head: current head address
- * @n: number to fill the node
- * Return: number of nodes.
- */
-listint_t *add_nodeint_end(listint_t **head, const int n)
-{
-	listint_t *new_node;
-	listint_t *last = *head;
-	/* In case it isn't imput a head*/
-	if (head == NULL)
-		return (NULL);
-	/*Allocate the new node*/
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
-	/*Assign info to the new node*/
-	new_node->n = n;
-	new_node->next = NULL;
-	/*Assign the head if the list is empty*/
-	if (*head == NULL)
-	{
-		*head = new_node;
-		return (new_node);
-	}
-	/*If the list isn't empty, find the last node and connect the new node*/
-	while (last->next != NULL)
-		last = last->next;
-	last->next = new_node;
 
-	return (new_node);
+/**
+ * free_listint - function with one argument
+ * @head: pointer to node
+ *
+ * Description: frees a list
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *traverse;
+
+	while (head)
+	{
+		traverse = head->next;
+		free(head);
+		head = traverse;
+	}
 }
